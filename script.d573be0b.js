34aadf800a8da695ecb6442be988386fcd3dbd18
RonanDrouglazet <ronan.drouglazet@teads.tv>
2021-01-08 00:51:30 +0100

    update

--- script.d573be0b.js
+++ script.d573be0b.js
@@ -293,7 +293,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50973" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51657" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
